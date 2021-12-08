90feed436e921fc6cd65154a6fa74d29d5b2ef6e
PPTaa <chobo05@naver.com>
2021-12-08 20:36:19 +0900

    Initial Commit

---
+++ MyCalcApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCalcApp
+//
+//  Created by leejungchul on 2021/12/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
